481c1202d17540e0013dad061e0ad21756976f56
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2025-08-13 12:15:19 +0100

    added calc discount function

--- calc_discount.py
+++ calc_discount.py
@@ -3,6 +3,4 @@ def calculate_discount(price, discount_percent):
     if discount_percent >= 20:
         final_price = price * (1 - (discount_percent / 100))
     else: final_price = price
-    return final_price
-
-print(calculate_discount(1000, 10))
+    print(final_price)
